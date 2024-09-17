8fe3557bd6d560a4ca9444806a83e2a071544d24
m-riss <95452643+m-riss@users.noreply.github.com>
2024-09-17 10:09:46 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -129,6 +129,189 @@ int _117 = 0;
 int _118 = 0;
 int _119 = 0;
 int _120 = 0;
+int _121 = 0;
+int _122 = 0;
+int _123 = 0;
+int _124 = 0;
+int _125 = 0;
+int _126 = 0;
+int _127 = 0;
+int _128 = 0;
+int _129 = 0;
+int _130 = 0;
+int _131 = 0;
+int _132 = 0;
+int _133 = 0;
+int _134 = 0;
+int _135 = 0;
+int _136 = 0;
+int _137 = 0;
+int _138 = 0;
+int _139 = 0;
+int _140 = 0;
+int _141 = 0;
+int _142 = 0;
+int _143 = 0;
+int _144 = 0;
+int _145 = 0;
+int _146 = 0;
+int _147 = 0;
+int _148 = 0;
+int _149 = 0;
+int _150 = 0;
+int _151 = 0;
+int _152 = 0;
+int _153 = 0;
+int _154 = 0;
+int _155 = 0;
+int _156 = 0;
+int _157 = 0;
+int _158 = 0;
+int _159 = 0;
+int _160 = 0;
+int _161 = 0;
+int _162 = 0;
+int _163 = 0;
+int _164 = 0;
+int _165 = 0;
+int _166 = 0;
+int _167 = 0;
+int _168 = 0;
+int _169 = 0;
+int _170 = 0;
+int _171 = 0;
+int _172 = 0;
+int _173 = 0;
+int _174 = 0;
+int _175 = 0;
+int _176 = 0;
+int _177 = 0;
+int _178 = 0;
+int _179 = 0;
+int _180 = 0;
+int _181 = 0;
+int _182 = 0;
+int _183 = 0;
+int _184 = 0;
+int _185 = 0;
+int _186 = 0;
+int _187 = 0;
+int _188 = 0;
+int _189 = 0;
+int _190 = 0;
+int _191 = 0;
+int _192 = 0;
+int _193 = 0;
+int _194 = 0;
+int _195 = 0;
+int _196 = 0;
+int _197 = 0;
+int _198 = 0;
+int _199 = 0;
+int _200 = 0;
+int _201 = 0;
+int _202 = 0;
+int _203 = 0;
+int _204 = 0;
+int _205 = 0;
+int _206 = 0;
+int _207 = 0;
+int _208 = 0;
+int _209 = 0;
+int _210 = 0;
+int _211 = 0;
+int _212 = 0;
+int _213 = 0;
+int _214 = 0;
+int _215 = 0;
+int _216 = 0;
+int _217 = 0;
+int _218 = 0;
+int _219 = 0;
+int _220 = 0;
+int _221 = 0;
+int _222 = 0;
+int _223 = 0;
+int _224 = 0;
+int _225 = 0;
+int _226 = 0;
+int _227 = 0;
+int _228 = 0;
+int _229 = 0;
+int _230 = 0;
+int _231 = 0;
+int _232 = 0;
+int _233 = 0;
+int _234 = 0;
+int _235 = 0;
+int _236 = 0;
+int _237 = 0;
+int _238 = 0;
+int _239 = 0;
+int _240 = 0;
+int _241 = 0;
+int _242 = 0;
+int _243 = 0;
+int _244 = 0;
+int _245 = 0;
+int _246 = 0;
+int _247 = 0;
+int _248 = 0;
+int _249 = 0;
+int _250 = 0;
+int _251 = 0;
+int _252 = 0;
+int _253 = 0;
+int _254 = 0;
+int _255 = 0;
+int _256 = 0;
+int _257 = 0;
+int _258 = 0;
+int _259 = 0;
+int _260 = 0;
+int _261 = 0;
+int _262 = 0;
+int _263 = 0;
+int _264 = 0;
+int _265 = 0;
+int _266 = 0;
+int _267 = 0;
+int _268 = 0;
+int _269 = 0;
+int _270 = 0;
+int _271 = 0;
+int _272 = 0;
+int _273 = 0;
+int _274 = 0;
+int _275 = 0;
+int _276 = 0;
+int _277 = 0;
+int _278 = 0;
+int _279 = 0;
+int _280 = 0;
+int _281 = 0;
+int _282 = 0;
+int _283 = 0;
+int _284 = 0;
+int _285 = 0;
+int _286 = 0;
+int _287 = 0;
+int _288 = 0;
+int _289 = 0;
+int _290 = 0;
+int _291 = 0;
+int _292 = 0;
+int _293 = 0;
+int _294 = 0;
+int _295 = 0;
+int _296 = 0;
+int _297 = 0;
+int _298 = 0;
+int _299 = 0;
+int _300 = 0;
+
+
+
 
 
 int number = 0;
@@ -258,7 +441,186 @@ _117 = 117;
 _118 = 118;
 _119 = 119;
 _120 = 120;
-
+_121 = 121;
+_122 = 122;
+_123 = 123;
+_124 = 124;
+_125 = 125;
+_126 = 126;
+_127 = 127;
+_128 = 128;
+_129 = 129;
+_130 = 130;
+_131 = 131;
+_132 = 132;
+_133 = 133;
+_134 = 134;
+_135 = 135;
+_136 = 136;
+_137 = 137;
+_138 = 138;
+_139 = 139;
+_140 = 140;
+_141 = 141;
+_142 = 142;
+_143 = 143;
+_144 = 144;
+_145 = 145;
+_146 = 146;
+_147 = 147;
+_148 = 148;
+_149 = 149;
+_150 = 150;
+_151 = 151;
+_152 = 152;
+_153 = 153;
+_154 = 154;
+_155 = 155;
+_156 = 156;
+_157 = 157;
+_158 = 158;
+_159 = 159;
+_160 = 160;
+_161 = 161;
+_162 = 162;
+_163 = 163;
+_164 = 164;
+_165 = 165;
+_166 = 166;
+_167 = 167;
+_168 = 168;
+_169 = 169;
+_170 = 170;
+_171 = 171;
+_172 = 172;
+_173 = 173;
+_174 = 174;
+_175 = 175;
+_176 = 176;
+_177 = 177;
+_178 = 178;
+_179 = 179;
+_180 = 180;
+_181 = 181;
+_182 = 182;
+_183 = 183;
+_184 = 184;
+_185 = 185;
+_186 = 186;
+_187 = 187;
+_188 = 188;
+_189 = 189;
+_190 = 190;
+_191 = 191;
+_192 = 192;
+_193 = 193;
+_194 = 194;
+_195 = 195;
+_196 = 196;
+_197 = 197;
+_198 = 198;
+_199 = 199;
+_200 = 200;
+_201 = 201;
+_202 = 202;
+_203 = 203;
+_204 = 204;
+_205 = 205;
+_206 = 206;
+_207 = 207;
+_208 = 208;
+_209 = 209;
+_210 = 210;
+_211 = 211;
+_212 = 212;
+_213 = 213;
+_214 = 214;
+_215 = 215;
+_216 = 216;
+_217 = 217;
+_218 = 218;
+_219 = 219;
+_220 = 220;
+_221 = 221;
+_222 = 222;
+_223 = 223;
+_224 = 224;
+_225 = 225;
+_226 = 226;
+_227 = 227;
+_228 = 228;
+_229 = 229;
+_230 = 230;
+_231 = 231;
+_232 = 232;
+_233 = 233;
+_234 = 234;
+_235 = 235;
+_236 = 236;
+_237 = 237;
+_238 = 238;
+_239 = 239;
+_240 = 240;
+_241 = 241;
+_242 = 242;
+_243 = 243;
+_244 = 244;
+_245 = 245;
+_246 = 246;
+_247 = 247;
+_248 = 248;
+_249 = 249;
+_250 = 250;
+_251 = 251;
+_252 = 252;
+_253 = 253;
+_254 = 254;
+_255 = 255;
+_256 = 256;
+_257 = 257;
+_258 = 258;
+_259 = 259;
+_260 = 260;
+_261 = 261;
+_262 = 262;
+_263 = 263;
+_264 = 264;
+_265 = 265;
+_266 = 266;
+_267 = 267;
+_268 = 268;
+_269 = 269;
+_270 = 270;
+_271 = 271;
+_272 = 272;
+_273 = 273;
+_274 = 274;
+_275 = 275;
+_276 = 276;
+_277 = 277;
+_278 = 278;
+_279 = 279;
+_280 = 280;
+_281 = 281;
+_282 = 282;
+_283 = 283;
+_284 = 284;
+_285 = 285;
+_286 = 286;
+_287 = 287;
+_288 = 288;
+_289 = 289;
+_290 = 290;
+_291 = 291;
+_292 = 292;
+_293 = 293;
+_294 = 294;
+_295 = 295;
+_296 = 296;
+_297 = 297;
+_298 = 298;
+_299 = 299;
+_300 = 300;
 
 Odd = "Odd";
 Even = "Even";
@@ -508,165 +870,445 @@ if (number == _119) {
 cout << Odd << endl; }
 if (number == _120) {
 cout << Even << endl; }
-if (number == 121) {
+if (number == _121) {
 cout << Odd << endl; }
-if (number == 122) {
+if (number == _122) {
 cout << Even << endl; }
-if (number == 123) {
+if (number == _123) {
 cout << Odd << endl; }
-if (number == 124) {
+if (number == _124) {
 cout << Even << endl; }
-if (number == 125) {
+if (number == _125) {
 cout << Odd << endl; }
-if (number == 126) {
+if (number == _126) {
 cout << Even << endl; }
-if (number == 127) {
+if (number == _127) {
 cout << Odd << endl; }
-if (number == 128) {
+if (number == _128) {
 cout << Even << endl; }
-if (number == 129) {
+if (number == _129) {
 cout << Odd << endl; }
-if (number == 130) {
+if (number == _130) {
 cout << Even << endl; }
-if (number == 131) {
+if (number == _131) {
 cout << Odd << endl; }
-if (number == 132) {
+if (number == _132) {
 cout << Even << endl; }
-if (number == 133) {
+if (number == _133) {
 cout << Odd << endl; }
-if (number == 134) {
+if (number == _134) {
 cout << Even << endl; }
-if (number == 135) {
+if (number == _135) {
 cout << Odd << endl; }
-if (number == 136) {
+if (number == _136) {
 cout << Even << endl; }
-if (number == 137) {
+if (number == _137) {
 cout << Odd << endl; }
-if (number == 138) {
+if (number == _138) {
 cout << Even << endl; }
-if (number == 139) {
+if (number == _139) {
 cout << Odd << endl; }
-if (number == 140) {
+if (number == _140) {
 cout << Even << endl; }
-if (number == 141) {
+if (number == _141) {
 cout << Odd << endl; }
-if (number == 142) {
+if (number == _142) {
 cout << Even << endl; }
-if (number == 143) {
+if (number == _143) {
 cout << Odd << endl; }
-if (number == 144) {
+if (number == _144) {
 cout << Even << endl; }
-if (number == 145) {
+if (number == _145) {
 cout << Odd << endl; }
-if (number == 146) {
+if (number == _146) {
 cout << Even << endl; }
-if (number == 147) {
+if (number == _147) {
 cout << Odd << endl; }
-if (number == 148) {
+if (number == _148) {
 cout << Even << endl; }
-if (number == 149) {
+if (number == _149) {
 cout << Odd << endl; }
-if (number == 150) {
+if (number == _150) {
 cout << Even << endl; }
-if (number == 151) {
+if (number == _151) {
 cout << Odd << endl; }
-if (number == 152) {
+if (number == _152) {
 cout << Even << endl; }
-if (number == 153) {
+if (number == _153) {
 cout << Odd << endl; }
-if (number == 154) {
+if (number == _154) {
 cout << Even << endl; }
-if (number == 155) {
+if (number == _155) {
 cout << Odd << endl; }
-if (number == 156) {
+if (number == _156) {
 cout << Even << endl; }
-if (number == 157) {
+if (number == _157) {
 cout << Odd << endl; }
-if (number == 158) {
+if (number == _158) {
 cout << Even << endl; }
-if (number == 159) {
+if (number == _159) {
 cout << Odd << endl; }
-if (number == 160) {
+if (number == _160) {
 cout << Even << endl; }
-if (number == 161) {
+if (number == _161) {
 cout << Odd << endl; }
-if (number == 162) {
+if (number == _162) {
 cout << Even << endl; }
-if (number == 163) {
+if (number == _163) {
 cout << Odd << endl; }
-if (number == 164) {
+if (number == _164) {
 cout << Even << endl; }
-if (number == 165) {
+if (number == _165) {
 cout << Odd << endl; }
-if (number == 166) {
+if (number == _166) {
 cout << Even << endl; }
-if (number == 167) {
+if (number == _167) {
 cout << Odd << endl; }
-if (number == 168) {
+if (number == _168) {
 cout << Even << endl; }
-if (number == 169) {
+if (number == _169) {
 cout << Odd << endl; }
-if (number == 170) {
+if (number == _170) {
 cout << Even << endl; }
-if (number == 171) {
+if (number == _171) {
 cout << Odd << endl; }
-if (number == 172) {
+if (number == _172) {
 cout << Even << endl; }
-if (number == 173) {
+if (number == _173) {
 cout << Odd << endl; }
-if (number == 174) {
+if (number == _174) {
 cout << Even << endl; }
-if (number == 175) {
+if (number == _175) {
 cout << Odd << endl; }
-if (number == 176) {
+if (number == _176) {
 cout << Even << endl; }
-if (number == 177) {
+if (number == _177) {
 cout << Odd << endl; }
-if (number == 178) {
+if (number == _178) {
 cout << Even << endl; }
-if (number == 179) {
+if (number == _179) {
 cout << Odd << endl; }
-if (number == 180) {
+if (number == _180) {
 cout << Even << endl; }
-if (number == 181) {
+if (number == _181) {
 cout << Odd << endl; }
-if (number == 182) {
+if (number == _182) {
 cout << Even << endl; }
-if (number == 183) {
+if (number == _183) {
 cout << Odd << endl; }
-if (number == 184) {
+if (number == _184) {
 cout << Even << endl; }
-if (number == 185) {
+if (number == _185) {
 cout << Odd << endl; }
-if (number == 186) {
+if (number == _186) {
 cout << Even << endl; }
-if (number == 187) {
+if (number == _187) {
 cout << Odd << endl; }
-if (number == 188) {
+if (number == _188) {
 cout << Even << endl; }
-if (number == 189) {
+if (number == _189) {
 cout << Odd << endl; }
-if (number == 190) {
+if (number == _190) {
 cout << Even << endl; }
-if (number == 191) {
+if (number == _191) {
 cout << Odd << endl; }
-if (number == 192) {
+if (number == _192) {
 cout << Even << endl; }
-if (number == 193) {
+if (number == _193) {
 cout << Odd << endl; }
-if (number == 194) {
+if (number == _194) {
 cout << Even << endl; }
-if (number == 195) {
+if (number == _195) {
 cout << Odd << endl; }
-if (number == 196) {
+if (number == _196) {
 cout << Even << endl; }
-if (number == 197) {
+if (number == _197) {
 cout << Odd << endl; }
-if (number == 198) {
+if (number == _198) {
 cout << Even << endl; }
-if (number == 199) {
+if (number == _199) {
 cout << Odd << endl; }
-if (number == 200) {
+if (number == _200) {
+cout << Even << endl; }
+if (number == 201) {
+cout << Odd << endl; }
+if (number == 202) {
+cout << Even << endl; }
+if (number == 203) {
+cout << Odd << endl; }
+if (number == 204) {
+cout << Even << endl; }
+if (number == 205) {
+cout << Odd << endl; }
+if (number == 206) {
+cout << Even << endl; }
+if (number == 207) {
+cout << Odd << endl; }
+if (number == 208) {
+cout << Even << endl; }
+if (number == 209) {
+cout << Odd << endl; }
+if (number == 210) {
+cout << Even << endl; }
+if (number == 211) {
+cout << Odd << endl; }
+if (number == 212) {
+cout << Even << endl; }
+if (number == 213) {
+cout << Odd << endl; }
+if (number == 214) {
+cout << Even << endl; }
+if (number == 215) {
+cout << Odd << endl; }
+if (number == 216) {
+cout << Even << endl; }
+if (number == 217) {
+cout << Odd << endl; }
+if (number == 218) {
+cout << Even << endl; }
+if (number == 219) {
+cout << Odd << endl; }
+if (number == 220) {
+cout << Even << endl; }
+if (number == 221) {
+cout << Odd << endl; }
+if (number == 222) {
+cout << Even << endl; }
+if (number == 223) {
+cout << Odd << endl; }
+if (number == 224) {
+cout << Even << endl; }
+if (number == 225) {
+cout << Odd << endl; }
+if (number == 226) {
+cout << Even << endl; }
+if (number == 227) {
+cout << Odd << endl; }
+if (number == 228) {
+cout << Even << endl; }
+if (number == 229) {
+cout << Odd << endl; }
+if (number == 230) {
+cout << Even << endl; }
+if (number == 231) {
+cout << Odd << endl; }
+if (number == 232) {
+cout << Even << endl; }
+if (number == 233) {
+cout << Odd << endl; }
+if (number == 234) {
+cout << Even << endl; }
+if (number == 235) {
+cout << Odd << endl; }
+if (number == 236) {
+cout << Even << endl; }
+if (number == 237) {
+cout << Odd << endl; }
+if (number == 238) {
+cout << Even << endl; }
+if (number == 239) {
+cout << Odd << endl; }
+if (number == 240) {
+cout << Even << endl; }
+if (number == 241) {
+cout << Odd << endl; }
+if (number == 242) {
+cout << Even << endl; }
+if (number == 243) {
+cout << Odd << endl; }
+if (number == 244) {
+cout << Even << endl; }
+if (number == 245) {
+cout << Odd << endl; }
+if (number == 246) {
+cout << Even << endl; }
+if (number == 247) {
+cout << Odd << endl; }
+if (number == 248) {
+cout << Even << endl; }
+if (number == 249) {
+cout << Odd << endl; }
+if (number == 250) {
+cout << Even << endl; }
+if (number == 251) {
+cout << Odd << endl; }
+if (number == 252) {
+cout << Even << endl; }
+if (number == 253) {
+cout << Odd << endl; }
+if (number == 254) {
+cout << Even << endl; }
+if (number == 255) {
+cout << Odd << endl; }
+if (number == 256) {
+cout << Even << endl; }
+if (number == 257) {
+cout << Odd << endl; }
+if (number == 258) {
+cout << Even << endl; }
+if (number == 259) {
+cout << Odd << endl; }
+if (number == 260) {
+cout << Even << endl; }
+if (number == 261) {
+cout << Odd << endl; }
+if (number == 262) {
+cout << Even << endl; }
+if (number == 263) {
+cout << Odd << endl; }
+if (number == 264) {
+cout << Even << endl; }
+if (number == 265) {
+cout << Odd << endl; }
+if (number == 266) {
+cout << Even << endl; }
+if (number == 267) {
+cout << Odd << endl; }
+if (number == 268) {
+cout << Even << endl; }
+if (number == 269) {
+cout << Odd << endl; }
+if (number == 270) {
+cout << Even << endl; }
+if (number == 271) {
+cout << Odd << endl; }
+if (number == 272) {
+cout << Even << endl; }
+if (number == 273) {
+cout << Odd << endl; }
+if (number == 274) {
+cout << Even << endl; }
+if (number == 275) {
+cout << Odd << endl; }
+if (number == 276) {
+cout << Even << endl; }
+if (number == 277) {
+cout << Odd << endl; }
+if (number == 278) {
+cout << Even << endl; }
+if (number == 279) {
+cout << Odd << endl; }
+if (number == 280) {
+cout << Even << endl; }
+if (number == 281) {
+cout << Odd << endl; }
+if (number == 282) {
+cout << Even << endl; }
+if (number == 283) {
+cout << Odd << endl; }
+if (number == 284) {
+cout << Even << endl; }
+if (number == 285) {
+cout << Odd << endl; }
+if (number == 286) {
+cout << Even << endl; }
+if (number == 287) {
+cout << Odd << endl; }
+if (number == 288) {
+cout << Even << endl; }
+if (number == 289) {
+cout << Odd << endl; }
+if (number == 290) {
+cout << Even << endl; }
+if (number == 291) {
+cout << Odd << endl; }
+if (number == 292) {
+cout << Even << endl; }
+if (number == 293) {
+cout << Odd << endl; }
+if (number == 294) {
+cout << Even << endl; }
+if (number == 295) {
+cout << Odd << endl; }
+if (number == 296) {
+cout << Even << endl; }
+if (number == 297) {
+cout << Odd << endl; }
+if (number == 298) {
+cout << Even << endl; }
+if (number == 299) {
+cout << Odd << endl; }
+if (number == 300) {
+cout << Even << endl; }
+if (number == 301) {
+cout << Odd << endl; }
+if (number == 302) {
+cout << Even << endl; }
+if (number == 303) {
+cout << Odd << endl; }
+if (number == 304) {
+cout << Even << endl; }
+if (number == 305) {
+cout << Odd << endl; }
+if (number == 306) {
+cout << Even << endl; }
+if (number == 307) {
+cout << Odd << endl; }
+if (number == 308) {
+cout << Even << endl; }
+if (number == 309) {
+cout << Odd << endl; }
+if (number == 310) {
+cout << Even << endl; }
+if (number == 311) {
+cout << Odd << endl; }
+if (number == 312) {
+cout << Even << endl; }
+if (number == 313) {
+cout << Odd << endl; }
+if (number == 314) {
+cout << Even << endl; }
+if (number == 315) {
+cout << Odd << endl; }
+if (number == 316) {
+cout << Even << endl; }
+if (number == 317) {
+cout << Odd << endl; }
+if (number == 318) {
+cout << Even << endl; }
+if (number == 319) {
+cout << Odd << endl; }
+if (number == 320) {
+cout << Even << endl; }
+if (number == 321) {
+cout << Odd << endl; }
+if (number == 322) {
+cout << Even << endl; }
+if (number == 323) {
+cout << Odd << endl; }
+if (number == 324) {
+cout << Even << endl; }
+if (number == 325) {
+cout << Odd << endl; }
+if (number == 326) {
+cout << Even << endl; }
+if (number == 327) {
+cout << Odd << endl; }
+if (number == 328) {
+cout << Even << endl; }
+if (number == 329) {
+cout << Odd << endl; }
+if (number == 330) {
+cout << Even << endl; }
+if (number == 331) {
+cout << Odd << endl; }
+if (number == 332) {
+cout << Even << endl; }
+if (number == 333) {
+cout << Odd << endl; }
+if (number == 334) {
+cout << Even << endl; }
+if (number == 335) {
+cout << Odd << endl; }
+if (number == 336) {
+cout << Even << endl; }
+if (number == 337) {
+cout << Odd << endl; }
+if (number == 338) {
+cout << Even << endl; }
+if (number == 339) {
+cout << Odd << endl; }
+if (number == 340) {
 cout << Even << endl; }
 
 cin >> Exit;
@@ -791,6 +1433,186 @@ _117 = 0;
 _118 = 0;
 _119 = 0;
 _120 = 0;
+_121 = 0;
+_122 = 0;
+_123 = 0;
+_124 = 0;
+_125 = 0;
+_126 = 0;
+_127 = 0;
+_128 = 0;
+_129 = 0;
+_130 = 0;
+_131 = 0;
+_132 = 0;
+_133 = 0;
+_134 = 0;
+_135 = 0;
+_136 = 0;
+_137 = 0;
+_138 = 0;
+_139 = 0;
+_140 = 0;
+_141 = 0;
+_142 = 0;
+_143 = 0;
+_144 = 0;
+_145 = 0;
+_146 = 0;
+_147 = 0;
+_148 = 0;
+_149 = 0;
+_150 = 0;
+_151 = 0;
+_152 = 0;
+_153 = 0;
+_154 = 0;
+_155 = 0;
+_156 = 0;
+_157 = 0;
+_158 = 0;
+_159 = 0;
+_160 = 0;
+_161 = 0;
+_162 = 0;
+_163 = 0;
+_164 = 0;
+_165 = 0;
+_166 = 0;
+_167 = 0;
+_168 = 0;
+_169 = 0;
+_170 = 0;
+_171 = 0;
+_172 = 0;
+_173 = 0;
+_174 = 0;
+_175 = 0;
+_176 = 0;
+_177 = 0;
+_178 = 0;
+_179 = 0;
+_180 = 0;
+_181 = 0;
+_182 = 0;
+_183 = 0;
+_184 = 0;
+_185 = 0;
+_186 = 0;
+_187 = 0;
+_188 = 0;
+_189 = 0;
+_190 = 0;
+_191 = 0;
+_192 = 0;
+_193 = 0;
+_194 = 0;
+_195 = 0;
+_196 = 0;
+_197 = 0;
+_198 = 0;
+_199 = 0;
+_200 = 0;
+_201 = 0;
+_202 = 0;
+_203 = 0;
+_204 = 0;
+_205 = 0;
+_206 = 0;
+_207 = 0;
+_208 = 0;
+_209 = 0;
+_210 = 0;
+_211 = 0;
+_212 = 0;
+_213 = 0;
+_214 = 0;
+_215 = 0;
+_216 = 0;
+_217 = 0;
+_218 = 0;
+_219 = 0;
+_220 = 0;
+_221 = 0;
+_222 = 0;
+_223 = 0;
+_224 = 0;
+_225 = 0;
+_226 = 0;
+_227 = 0;
+_228 = 0;
+_229 = 0;
+_230 = 0;
+_231 = 0;
+_232 = 0;
+_233 = 0;
+_234 = 0;
+_235 = 0;
+_236 = 0;
+_237 = 0;
+_238 = 0;
+_239 = 0;
+_240 = 0;
+_241 = 0;
+_242 = 0;
+_243 = 0;
+_244 = 0;
+_245 = 0;
+_246 = 0;
+_247 = 0;
+_248 = 0;
+_249 = 0;
+_250 = 0;
+_251 = 0;
+_252 = 0;
+_253 = 0;
+_254 = 0;
+_255 = 0;
+_256 = 0;
+_257 = 0;
+_258 = 0;
+_259 = 0;
+_260 = 0;
+_261 = 0;
+_262 = 0;
+_263 = 0;
+_264 = 0;
+_265 = 0;
+_266 = 0;
+_267 = 0;
+_268 = 0;
+_269 = 0;
+_270 = 0;
+_271 = 0;
+_272 = 0;
+_273 = 0;
+_274 = 0;
+_275 = 0;
+_276 = 0;
+_277 = 0;
+_278 = 0;
+_279 = 0;
+_280 = 0;
+_281 = 0;
+_282 = 0;
+_283 = 0;
+_284 = 0;
+_285 = 0;
+_286 = 0;
+_287 = 0;
+_288 = 0;
+_289 = 0;
+_290 = 0;
+_291 = 0;
+_292 = 0;
+_293 = 0;
+_294 = 0;
+_295 = 0;
+_296 = 0;
+_297 = 0;
+_298 = 0;
+_299 = 0;
+_300 = 0;
 number = 0;
 Exit = 0;
 
